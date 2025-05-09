65a56eb7a1e57a36e346ec9b1553a0364c237a1f
marlonvess <marlonvess@gmail.com>
2025-05-09 18:28:04 +0100

    update html css js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-a
+document.addEventListener('DOMContentLoaded', () => {
+    const tabButtons = document.querySelectorAll('.tab-btn');
+    const pages = document.querySelectorAll('.tab-content');
+  
+    tabButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const target = button.getAttribute('data-tab');
+  
+        pages.forEach(page => {
+          page.style.display = page.id === target ? 'block' : 'none';
+        });
+  
+        tabButtons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+      });
+    });
+  });
+  
